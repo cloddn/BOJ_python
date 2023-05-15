2353b8c9b64b36236c8441fd9a1292ea913affbe
cloddn <khj4849@soongsil.ac.kr>
2023-05-16 00:35:31 +0900

    230516

--- 백트레킹/N과 M(1).py
+++ 백트레킹/N과 M(1).py
@@ -1,16 +1,18 @@
-n,m = list(map(int,input().split()))
- 
-s = []
- 
+n,m = map(int, input().split()) 
+
+s=[]
+
 def dfs():
+
+    
     if len(s)==m:
-        print(' '.join(map(str,s)))
+        print(" ".join(map(str,s))) #s에 필요한 갯수 m개 다 넣으면 -> join(map(str,s))-> 1 3 출력되도록
         return
     
     for i in range(1,n+1):
         if i not in s:
             s.append(i)
-            dfs()
-            s.pop()
- 
+            dfs() #맨처음 1 2 나올때까지
+            s.pop()         
+            
 dfs()
