7d01827b9592ca16177b084c9eb256cb8d923ac8
cloddn <khj4849@soongsil.ac.kr>
2023-05-16 18:12:19 +0900

    230516

---
+++ 백트레킹/N과 M(4).py
@@ -0,0 +1,19 @@
+from collections import deque
+n,m = map(int, input().split()) 
+
+s=[]
+
+#시작 시점이 중요하므로 dfs(s) : 1부터 n까지 쓰는 것이 아니고 , 시작 지점부터 n까지 쓰기때문에
+def dfs(start):
+
+
+    if len(s)==m:
+        print(" ".join(map(str,s)))
+        return
+    
+    for i in range(start,n+1):#1에서 시작하는게 아니고 기준 점 start에서 더해질 수 있도록 
+            s.append(i) 
+            dfs(i)
+            s.pop()
+            
+dfs(1)
