c515a8864d3e54cf0f9f0051b1e0deee6db48ebb
cloddn <khj4849@soongsil.ac.kr>
2023-08-01 18:08:41 +0900

    230801 길라잡이

---
+++ 길라잡이/10828_스택.py
@@ -0,0 +1,26 @@
+import sys
+input=sys.stdin.readline
+k=int(input())
+stackList=[]
+for i in range(k):
+    s=input().split()
+    if s[0]=='push':
+        stackList.append(s[1])
+    elif s[0]=='top':
+        if len(stackList)==0:
+            print(-1)
+        else:
+            print(stackList[-1])
+    elif s[0]=='size':
+        print(len(stackList))
+    elif s[0]=='empty':
+        if len(stackList)==0:
+            print(1)
+        else:
+            print(0)
+    else:
+        if len(stackList)>0:
+            print(stackList.pop())  
+        else:
+            print(-1)
+        
